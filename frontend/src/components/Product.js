00199186f5b0960db57788e8937c08d65deef91a
Firstname Lastname <your_email@youremail.com>
2021-10-05 16:28:58 +0100

    prep to updload

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Card } from "react-bootstrap";
+import Rating from "./Rating";
+
+const Product = ({ product }) => {
+  return (
+    <Card className='my-3 p-3 rounded' style={{ height: "400px" }}>
+      <Link to={`/product/${product._id}`}>
+        <Card.Img
+          src={product.image}
+          variant='top'
+          style={{ height: "200px" }}
+        />
+      </Link>
+
+      <Card.Body>
+        <Link to={`/product/${product._id}`}>
+          <Card.Title as='div'>
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </Link>
+
+        <Card.Text as='div'>
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
+        </Card.Text>
+
+        <Card.Text as='h3'>${product.price}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Product;
